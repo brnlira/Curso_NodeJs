0d278890e3817b0b4148aec99901b449f89ed68c
Bruno Marques Lira <brunomarqueslira@outlook.com>
2021-07-29 05:10:55 -0300

    Respondendo requisições com base na URL requisitada

--- noticias.js
+++ noticias.js
@@ -2,8 +2,17 @@ var http = require('http');
 
 var server = http.createServer( function(req, res){
 
-    res.end("<html><body>Portal de notícias</body></http>");
+    var categoria = req.url;
 
+    if(categoria == '/tecnologia'){
+        res.end("<html><body>Portal de notícias de Tecnologia</body></http>");
+    } else if (categoria == '/moda') {
+        res.end("<html><body>Portal de notícias de Moda</body></http>");
+    } else if (categoria == '/beleza') {
+        res.end("<html><body>Portal de notícias de Beleza</body></http>");
+    } else {
+        res.end("<html><body>Portal de notícias</body></http>");
+    }
 });
 
 server.listen(3000);
